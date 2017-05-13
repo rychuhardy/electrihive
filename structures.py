e4b04ce497764b9f64714fc3bc50e37c83854491
jakubtustanowski <tusty53@gmail.com>
2017-05-13 21:49:41 +0200

    Removing Node class - using default NetworkX implementation should be fine.

--- structures.py
+++ structures.py
@@ -1,16 +1,8 @@
 import networkx as nx
 
 
-class Node:
-    def __init__(self, node_id, cost=0.0):
-        self.node_id = node_id
-        self.cost = cost
-
-
 class Plant:
-    def __init__(self, node=None, cost=0.0):
-        if node is None:
-            node = Node(0)
+    def __init__(self, node, cost=0.0):
         self.node = node
         self.cost = cost
 
