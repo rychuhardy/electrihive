3cb295312d5737ca696f757ccfcfd87c43b43a88
Filip Galas <fgalas525@gmail.com>
2017-05-20 02:23:03 +0200

    Change model classes to be immutable and calculate costs during their initializations

--- src/structures.py
+++ src/structures.py
@@ -2,19 +2,53 @@ import networkx as nx
 
 
 class Plant:
-    def __init__(self, node, cost=0.0):
+    def __init__(self, node, power=0.0, cost=0.0):
         self.node = node
+        self.power = power
         self.cost = cost
+
+    def __hash__(self):
+        return hash((self.node, self.power))
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and other.node == self.node and other.power == self.power
+
+
+class ImmutableGraph(nx.Graph):
+    def __hash__(self):
+        return sum((hash(n) for n in self.nodes()))
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and nx.is_isomorphic(other, self)
 
 
 class Network:
     def __init__(self, graph=None, plant=None):
         if graph is None:
             graph = nx.Graph()
-        self.graph = graph
+        self.graph = ImmutableGraph(graph)
         if plant is None:
             plant = Plant()
         self.plant = plant
+        self.demand = self.calculate_demand()
+        self.cost = self.calculate_cost()
+
+    def calculate_demand(self):
+        return sum((n["demand"] for n in self.graph.node))
+
+    def calculate_cost(self):
+        cost = sum((n["cost"] for n in self.graph.node))
+        cost += self.plant.cost
+        return cost
+
+    def is_valid(self):
+        return self.plant.power >= self.demand
+
+    def __hash__(self):
+        return hash((self.graph, self.plant))
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and other.graph == self.graph and other.plant == self.plant
 
 
 class Solution:
@@ -22,5 +56,17 @@ class Solution:
         if network_list is None:
             network_list = []
         self.network_list = network_list
-        self.cost = None
-        self.times_used=0
+        self.cost = self.calculate_cost()
+        self.times_used = 0
+
+    def calculate_cost(self):
+        return sum((network.cost for network in self.network_list))
+
+    def is_valid(self):
+        return all((network.is_valid() for network in self.network_list))
+
+    def __hash__(self):
+        return sum((hash(network) for network in self.network_list))
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and all(network in other.network_list for network in self.network_list)
